dab245294284392e87d30a0854fb3243c0646e41
M Ulmer <mju9999@gmail.com>
2025-02-09 14:14:15 -0800

    Add basic logic to create new books and add them to library array

--- script.js
+++ script.js
@@ -2,10 +2,26 @@
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author) {
+    if (!new.target) {
+        throw Error('Book function should only be used as a constructor.');
+    }
+    this.title = title;
+    this.author = author;
+    this.read = false;
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+Book.prototype.isRead = function(value) { this.read = value}
+
+function addBookToLibrary(title, author) {
+  myLibrary.push(new Book(title, author));
 }
+
+
+addBookToLibrary("Pattern Recognition", "William Gibson");
+addBookToLibrary("Piranesi", "Susanna Clarke");
+addBookToLibrary("walkaway", "Cory Doctorow");
+
+myLibrary.at(1).isRead(true);
+
+console.log(myLibrary);
