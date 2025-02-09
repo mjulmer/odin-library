378079b00405e334db84d938fe7edf2c09f780b1
M Ulmer <mju9999@gmail.com>
2025-02-09 14:37:54 -0800

    Show books in library on the screen

--- script.js
+++ script.js
@@ -3,20 +3,38 @@
 const myLibrary = [];
 
 function Book(title, author) {
-    if (!new.target) {
-        throw Error('Book function should only be used as a constructor.');
-    }
-    this.title = title;
-    this.author = author;
-    this.read = false;
+  if (!new.target) {
+    throw Error("Book function should only be used as a constructor.");
+  }
+  this.title = title;
+  this.author = author;
+  this.read = false;
 }
 
-Book.prototype.isRead = function(value) { this.read = value}
+Book.prototype.isRead = function (value) {
+  this.read = value;
+};
 
 function addBookToLibrary(title, author) {
   myLibrary.push(new Book(title, author));
 }
 
+function addBooksToDom() {
+  for (const book of myLibrary) {
+    const bookElement = document.createElement("div");
+    bookElement.className = "book";
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
+
+    bookElement.appendChild(bookTitle);
+    bookElement.appendChild(bookAuthor);
+
+    const bookshelf = document.querySelector(".bookshelf");
+    bookshelf.appendChild(bookElement);
+  }
+}
 
 addBookToLibrary("Pattern Recognition", "William Gibson");
 addBookToLibrary("Piranesi", "Susanna Clarke");
@@ -24,4 +42,4 @@ addBookToLibrary("walkaway", "Cory Doctorow");
 
 myLibrary.at(1).isRead(true);
 
-console.log(myLibrary);
+addBooksToDom();
