24386965ce114c0eec88861ff6aecf650ebc8a98
M Ulmer <mju9999@gmail.com>
2025-02-09 19:37:08 -0800

    Add logic to actually add new books created with modal

--- script.js
+++ script.js
@@ -2,30 +2,38 @@
 
 const myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, read = false) {
   if (!new.target) {
     throw Error("Book function should only be used as a constructor.");
   }
   this.title = title;
   this.author = author;
-  this.read = false;
+  this.read = read;
 }
 
 Book.prototype.isRead = function (value) {
   this.read = value;
 };
 
-function addBookToLibrary(title, author) {
-  myLibrary.push(new Book(title, author));
+function addBookToLibrary(title, author, read = false) {
+  myLibrary.push(new Book(title, author, read));
+}
+
+function addBookToLibraryAndDom(title, author, read = false) {
+  const newBook = new Book(title, author, read);
+  myLibrary.push(newBook);
+  const bookshelf = document.querySelector(".bookshelf");
+  const newBookButton = document.querySelector(".newBookButton");
+  bookshelf.insertBefore(createNewBookHtmlItem(newBook), newBookButton);
 }
 
 function setUpUiElements() {
-    addBooksToDom();
-    addNewBookButton()
+  addBooksToDom();
+  addNewBookButton();
 }
 
 function addBooksToDom() {
-    const bookshelf = document.querySelector(".bookshelf");
+  const bookshelf = document.querySelector(".bookshelf");
   for (const book of myLibrary) {
     bookshelf.appendChild(createNewBookHtmlItem(book));
   }
@@ -34,27 +42,41 @@ function addBooksToDom() {
 // I don't know of the "right" way to keep something as the last element
 // so for now I'm manually maintaining the last position
 function addNewBookButton() {
-    const bookshelf = document.querySelector(".bookshelf");
-    
-    const newBookButton = document.createElement("button");
-    newBookButton.className = "newBookButton";
-    newBookButton.textContent = "Add new book";
+  const bookshelf = document.querySelector(".bookshelf");
 
-    bookshelf.appendChild(newBookButton);
+  const newBookButton = document.createElement("button");
+  newBookButton.className = "newBookButton";
+  newBookButton.textContent = "Add new book";
+  const newBookDialog = document.querySelector(".newBookDialog");
+  newBookButton.addEventListener("click", () => newBookDialog.showModal());
+
+  const submitButton = document.querySelector("#new-book-submit");
+  submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    addBookToLibraryAndDom(
+      document.querySelector("#title").value,
+      document.querySelector("#author").value,
+      document.querySelector("#read-toggle").checked
+    );
+    newBookDialog.close();
+  });
+
+  bookshelf.appendChild(newBookButton);
 }
 
 function createNewBookHtmlItem(book) {
-    const bookElement = document.createElement("div");
-    bookElement.className = "book";
-    const bookTitle = document.createElement("p");
-    bookTitle.textContent = book.title;
-    const bookAuthor = document.createElement("p");
-    bookAuthor.textContent = book.author;
+  const bookElement = document.createElement("div");
+  bookElement.className = "book";
+  const bookTitle = document.createElement("p");
+  bookTitle.textContent = book.title;
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
 
-    bookElement.appendChild(bookTitle);
-    bookElement.appendChild(bookAuthor);
+  bookElement.appendChild(bookTitle);
+  bookElement.appendChild(bookAuthor);
 
-    return bookElement;
+  return bookElement;
 }
 
 addBookToLibrary("Pattern Recognition", "William Gibson");
